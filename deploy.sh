f5e543a3d75f9780c66c718901a93a03c99364fd
singchan <billy321@im-paris.fr>
2017-04-20 16:16:52 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git mv dist/* ./
 git rm src package.json src deploy.sh
 git status
 git commit -am "build"
-git push -f origin master:gh-pages
+git push -f origin $(git branch | sed -n -e 's/^\* \(.*\)/\1/p'):gh-pages
 git status
 ls
