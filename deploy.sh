9cc4ed52e3f359a0bfaf7f319c258c7c0116f7b4
singchan <billy321@im-paris.fr>
2017-04-20 16:23:41 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 git config --global user.email "bot@internetdefrance.fr"
 git config --global user.name "Codeship CI"
+git checkout master
 git mv dist/* ./
 git rm src package.json src deploy.sh
 git commit -am "build"
 echo "------------"
-echo $(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-git push -f origin $(git branch | sed -n -e 's/^\* \(.*\)/\1/p'):gh-pages
+git push -f origin master:gh-pages
