2641f85da20e47be499d18115592d9bd5599e379
singchan <billy321@im-paris.fr>
2017-04-20 14:23:42 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 mkdir -p dist
 npm install
 npm run build
+git config --global user.email "bot@internetdefrance.fr"
+git config --global user.name "Codeship CI"
 git add dist
 git commit -am "build"
 git push origin master
