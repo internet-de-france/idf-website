8f348c57d6e7d5781fce76c1f68c81755bb3d1f3
singchan <billy321@im-paris.fr>
2017-04-20 15:46:18 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ git config --global user.name "Codeship CI"
 git mv dist/* ./
 git rm src package.json src deploy.sh
 git commit -am "build"
-git branch -D gh-pages
 git push -f origin master:gh-pages
+git status
+ls
